05192a4ad596ed8b977738ed8ec505a154951baf
Qaiser Ejaz <qaiserejaz125@gmail.com>
2025-02-27 11:18:51 +0500

    svg

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -170,7 +170,7 @@ const Home = () => {
   // Configuration for the Lottie animation
   const lottieOptions = {
     // src: "https://lottie.host/58753882-bb6a-49f5-a2c0-950eda1e135a/NLbpVqGegK.lottie",
-    src:"../src/animations/Coding.json",
+    src:"/Coding.json",
     // src:"/Coding4.lottie",
     // src :"https://lottie.host/314f2f76-1eee-4e9c-8a4a-9c533fe7568e/y5BCzVhs1I.lottie",
     // src:"https://lottie.host/f694b36f-85d3-4714-8552-59871431c8e8/29mWnfzAk9.lottie",
