26291b2485830268b31a50a1fa64283701bd2730
Qaiser Ejaz <qaiserejaz125@gmail.com>
2025-02-27 18:25:26 +0500

    revert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-    minify: 'terser', // Minify JS
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/database', 'firebase/firestore'],
-        },
-      },
-    },
-  },
-});
+})
+
